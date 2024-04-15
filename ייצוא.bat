6ffac2bb728888ade8d55beb02b5f9f924af9ae3
ori halevi <60349296+orihalevi@users.noreply.github.com>
2024-04-15 23:01:25 +0300

    g

--- ייצוא.bat
+++
@@ -1,5 +0,0 @@
-pyinstaller --onefile -w -i "bot.png" "Telegram windows command bot.py"
-move "dist\Telegram windows command bot.exe"
-rd /s /q build
-rd /s /q dist
-del "Telegram windows command bot.spec"
